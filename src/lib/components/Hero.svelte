58857a778572789aacbe3cc9b581c32b4dc4a362
engageintellect <jc9361@gmail.com>
2023-02-10 20:54:50 -0800

    cleaning up

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,15 +1,15 @@
 <div
-	class="hero h-[100vh] lg:h-[45rem] bg-base-100 hover:shadow-lg"
+	class="hero h-[100vh] lg:h-[45rem] shadow-lg"
 	style="background-image: url('https://images.unsplash.com/photo-1644325349124-d1756b79dd42?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2675&q=80');"
 >
 	<div class="hero-overlay bg-opacity-70" />
-	<div class="hero-content text-center text-neutral-content">
-		<div class="max-w-lg">
-			<h1 class="mb-5 text-8xl font-semibold">Svelte</h1>
-			<p class="mb-5">
+	<div class="hero-content text-neutral-content">
+		<div class="z-60 drop-shadow-lg text-center">
+			<div class="mb-5 text-8xl font-semibold">Svelte</div>
+			<div class="mb-5 max-w-sm">
 				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
 				quasi. In deleniti eaque aut repudiandae et a id nisi.
-			</p>
+			</div>
 			<div class="flex gap-4 justify-center">
 				<button class="btn btn-primary transition-color duration-300 text-lg">Learn More</button>
 				<button class="btn transition-color duration-300 text-lg">Get Started</button>
